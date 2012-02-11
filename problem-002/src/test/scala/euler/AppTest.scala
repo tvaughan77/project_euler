a98ec7d0718e554e2a54353a80a5137bfee9c091
tom.vaughan <tom_vaughan@hotmail.com>
2012-02-11 17:26:43 -0500

    Adding a winning correct solution for the 2nd problem with a tail-recursive fibonacci solution and some unit tests to make sure I got it right

--- problem-002/src/test/scala/euler/AppTest.scala
+++ problem-002/src/test/scala/euler/AppTest.scala
@@ -9,9 +9,40 @@ import org.junit.Assert._
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends FunSuite {
 
-  test("The sum of 1+1=2") {
-    assertEquals(2, 1+1)
+  test("A fibonacci list of numbers less than 0 is (0)") {
+    assertEquals(List(0), App.fibonacciList(0))
   }
+  
+  
+  test("A fibonacci list of numbers less than 1 is (1)") {
+    assertEquals(List(1), App.fibonacciList(1))
+  }
+  
+  
+  test("A fibonacci list of numbers less than 2 is (1, 2)") {
+    assertEquals(List(1, 2), App.fibonacciList(2))
+  }
+  
+  
+  test("A fibonacci list of numbers less than 20 is (1, 2, 3, 5, 8, 13)") {
+    assertEquals(List(1, 2, 3, 5, 8, 13), App.fibonacciList(20))
+  }
+  
+  
+  test("A fibonacci list of numbers less than 100 is (1, 2, 3, 5, 8, 13, 21, 34, 55, 89)") {
+    assertEquals(List(1, 2, 3, 5, 8, 13, 21, 34, 55, 89), App.fibonacciList(100))
+  }
+  
+  
+  test("The sum of even fibonacci numbers less than 20 is (2+8) = 10") {
+    assertEquals(10, App.sumEvenFibs(10))
+  }
+  
+  
+  test("The sum of even fibonacci numbers less than 100 is (2 + 8 + 34) = 44") {
+    assertEquals(44, App.sumEvenFibs(44))
+  }
+  
 }
 
 
