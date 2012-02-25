77227426ddca4db402e0f29c233ed5e412e6e8a6
tom.vaughan <tom_vaughan@hotmail.com>
2012-02-25 17:45:56 -0500

    Adding a comment about the right "math way" to do this

--- problem-005/src/main/scala/euler/App.scala
+++ problem-005/src/main/scala/euler/App.scala
@@ -5,6 +5,26 @@ package euler.problem005
  * 
  * <p>What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?</p>
  * 
+ * <p>As pointed out by one of the project euler solution solvers, the common sense math way to do this is:
+ * <pre>
+ * This does not require programming at all. Compute the prime factorization of each number from 1 to 20, and multiply 
+ * the greatest power of each prime together:
+ * 20 = 2^2 * 5
+ * 19 = 19
+ * 18 = 2 * 3^2
+ * 17 = 17
+ * 16 = 2^4
+ * 15 = 3 * 5
+ * 14 = 2 * 7
+ * 13 = 13
+ * 11 = 11
+ * 
+ * All others are included in the previous numbers.
+ * 
+ * ANSWER: 2^4 * 3^2 * 5 * 7 * 11 * 13 * 17 * 19 = 232 792 560
+ * </pre>
+ * </p>
+ * 
  * @since 2012/02/25
  */
 object App {
