b259d82448e0f87db33b8f46f3877820da3eec9f
tom.vaughan <tom_vaughan@hotmail.com>
2012-03-09 17:08:08 -0500

    Queuing up problems 9, 10 and 11 for my flight to Paris tomorrow.

---
+++ problem-009/src/main/scala/euler/App.scala
@@ -0,0 +1,16 @@
+package euler.problem009
+
+/**
+ * <p>A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,<br/>
+ * a^2 + b^2 = c^2
+ * </p>
+ * <p>For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.</p>
+ *
+ * <p>There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.</p>
+ */
+object App {
+
+  def main(args: Array[String]) {
+    
+  }
+}
