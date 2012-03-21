9bcb1581f8b64448047d1acddb3c763d33d305c6
tom.vaughan <tom_vaughan@hotmail.com>
2012-03-21 11:06:14 -0400

    Committing a solution that got me the right answer.  Couple of gross inefficiencies in here, and can't figure out (yet) how to filter a list of lists in a way where I want to drop (b, a, c) if there is already an (a, b, c)

--- problem-009/src/main/scala/euler/App.scala
+++ problem-009/src/main/scala/euler/App.scala
@@ -7,11 +7,37 @@ package euler.problem009
  * <p>For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.</p>
  *
  * <p>There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.</p>
+ * 
+ * <p>
+ * TODO / FIXMEs:<br>
+ * It's inefficient to test all tuples for pythagorean-triplet-ness.  For example, (1, 1, 998) is certainly not a triplet<br/>
+ * It's also inefficient to test (a, b, c) and (b, a, c)<br/>
+ * 
+ * </p>
  */
 object App {
 
   def main(args: Array[String]) {
+    val triplets = terms3(1000) filter (isPythagoreanTriplet(_))
+    println("The triplets in 1000 are = " + triplets)
     
+    val products = triplets map (_.product)
+    println("The product of the pythagorean triplets for which (a + b + c = 1000) is =" + products)
+    
+  }
+  
+  /**
+   * @param a A natural number (for the definition where "natural" is > 0)
+   * @return a List of all the trios of natural numbers that sum to {@code a}
+   * For example, {@code terms3(4)} returns {@code List(List(1, 1, 2), List(1, 2, 1), List(1, 1, 2))}
+   */
+  def terms3(a: Int): List[List[Int]] = {
+    require(a > 0)
+    var termsList = new scala.collection.mutable.ListBuffer[List[Int]]
+    for(i <- 1 until a) {
+      termsList ++= terms2(a - i) map (List(i) ::: _)
+    }
+    termsList.toList
   }
   
   /**
@@ -22,12 +48,28 @@ object App {
   def terms2(a: Int): List[List[Int]] = {
     require(a > 0)
     var termsList = new scala.collection.mutable.ListBuffer[List[Int]]
-    for(i <- 1 until (a)) {
+    for(i <- 1 until a) {
       termsList += List(i, a - i)
     }
     termsList.toList
   }
   
+  /**
+   * Same as {@code isPythagoreanTriplet(a: Int, b: Int, c: Int)}, just in convenient List form
+   * @param triplet a list of 3 Natural numbers
+   * @return true if the 0th element squared plus the 1st element squared equals the third element squared
+   */
+  def isPythagoreanTriplet(triplet: List[Int]): Boolean = {
+    require(triplet.size == 3)
+    isPythagoreanTriplet(triplet(0), triplet(1), triplet(2))
+  }
+  
+  /**
+   * @param a A natural number
+   * @param b A natural number
+   * @param c A natural number
+   * @return true, if a*a plus b*b is equal to c*c.  False otherwise
+   */
   def isPythagoreanTriplet(a: Int, b: Int, c: Int): Boolean = {
     ((a * a) + (b * b)) == (c * c)
   }
--- problem-009/src/test/scala/euler/AppSuite.scala
+++ problem-009/src/test/scala/euler/AppSuite.scala
@@ -17,12 +17,24 @@ class AppSuite extends FunSuite {
     assertFalse(App.isPythagoreanTriplet(3, 4, 6))
   }
   
-  test("The terms of 4 are [(1, 3), (2, 2), (3, 1)]") {
+  test("The List(3, 4, 5) is a pythagorean triplet") {
+    assertTrue(App.isPythagoreanTriplet(List(3, 4, 5)))
+  }
+  
+  test("The terms2 of 4 are [(1, 3), (2, 2), (3, 1)]") {
     val terms = App.terms2(4)
     assertEquals(3, terms.size)
     assertTrue(terms.contains(List(1, 3)))
     assertTrue(terms.contains(List(2, 2)))
     assertTrue(terms.contains(List(3, 1)))
+  }
+  
+  test("The terms3 of 4 are [(1, 1, 2), (1, 2, 1), (2, 1, 1)]") {
+    val terms = App.terms3(4)
+    assertEquals(3, terms.size)
+    assertTrue(terms.contains(List(1, 1, 2)))
+    assertTrue(terms.contains(List(1, 2, 1)))
+    assertTrue(terms.contains(List(2, 1, 1)))
   }
 }
 
