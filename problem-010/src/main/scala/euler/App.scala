3e1ced5798b62e2739065df955f1779b652b3e7a
tom.vaughan <tom_vaughan@hotmail.com>
2012-03-22 15:30:00 -0400

    Adding a solution to problem 10 The answer is 142913828922

--- problem-010/src/main/scala/euler/App.scala
+++ problem-010/src/main/scala/euler/App.scala
@@ -1,5 +1,7 @@
 package euler.problem010
 
+import euler.Primes
+
 /**
  * <p>The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.</p>
  * 
@@ -7,7 +9,15 @@ package euler.problem010
  */
 object App {
 
+  val limit = 2000000
+  
   def main(args: Array[String]) {
+    val primes = Primes.getPrimesUpTo(limit)
+    println("The primes less than " + limit + " are " + primes)
+    
+    val sum = primes.foldLeft(0L)(_ + _)
+    
+    println("The sum of primes less than " + limit + " is " + sum)
 
   }
 }
