0051b1af87a61467395834052674c3a7eb6aa5e5
tom.vaughan <tom_vaughan@hotmail.com>
2012-03-22 15:31:09 -0400

    Adding another elegant solution from hideki_0 from the project_euler boards

--- problem-010/src/main/scala/euler/App.scala
+++ problem-010/src/main/scala/euler/App.scala
@@ -21,3 +21,22 @@ object App {
 
   }
 }
+
+/*
+ * Check out this elegant solution from hideshi_o off the project_euler boards
+ * 
+import scala.collection.mutable.ListBuffer
+object P10 {
+  def main(args:Array[String]) {
+    val lb = ListBuffer[Long]()
+    var count = 2L  
+    while(count <= 2000000) {
+      if(!(lb.exists(count % _ == 0L))) lb += count
+      count = count + 1L
+    }   
+    val sum = lb.toList.foldLeft(0L)(_ + _)
+    println(sum)
+  }
+}
+
+ */
