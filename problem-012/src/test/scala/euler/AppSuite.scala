5e9f38833816955784a8ce9b41e0dd7e08ee27bb
tom.vaughan <tom_vaughan@hotmail.com>
2012-06-14 16:27:15 -0400

    Adding my initial implementation of triangle number computation

--- problem-012/src/test/scala/euler/AppSuite.scala
+++ problem-012/src/test/scala/euler/AppSuite.scala
@@ -9,9 +9,30 @@ import org.junit.Assert._
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends FunSuite {
 
-  test("The sum of 1+1=2") {
-    assertEquals(2, 1+1)
+  test("The 1st triangle number is 1") {
+    assertEquals(1, App.triangle(1))
+  }
+
+
+  test("The 2nd triangle number is 3") {
+    assertEquals(3, App.triangle(2))
+  }  
+
+
+  test("The 3rd triangle number is 6") {
+    assertEquals(6, App.triangle(3))
+  }
+
+
+  test("The 4th triangle number is 10") {
+    assertEquals(10, App.triangle(4))
+  }
+  
+  test("The 10th triangle number is 55") {
+    assertEquals(55, App.triangle(10))
+    assertEquals(45, App.triangle(9))   // this should be pre-cached because of our call to triangle(10)
   }
 }
+
 
 
