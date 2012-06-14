50c7ffac6c899454f5f89c13bd74d9f7428fb539
tom.vaughan <tom_vaughan@hotmail.com>
2012-06-14 16:28:57 -0400

    Moving the triangle number implementation over to my helper euler-support library

--- problem-012/src/main/scala/euler/App.scala
+++ problem-012/src/main/scala/euler/App.scala
@@ -31,31 +31,5 @@ object App {
     
   }
   
-  /**
-   * <p>Computing triangle numbers is a perfect dynamic programming problem: computing the Nth triangle
-   * number requires the computation of all the 0...(N-1)th numbers.  To do this in Scala without streams, I'm 
-   * using a ListBuffer where the index of the ListBuffer contains the triangle number associated to that N.</p>
-   * <p>This method seeds the 0th element with 0 and then for any other call to {@code triangle(n)} will search
-   * backwards until it finds a defined element at some index I.  Once a defined number is found at index I, the
-   * algorithm "walks forward" to the requested index N, accumulating and storing the answer in the backing 
-   * ListBuffer[I], ListBuffer[I+1], etc. until ListBuffer[N] is computed and returned.</p>
-   *  
-   * @param n The nth triangle number you want. 
-   * @return The nth triangle number.
-   */
-  def triangle(n: Int): Long = {
-    require(n >= 0)
-    
-    if(n == 0) {
-      triangleNumbers.insert(0, 0)
-    } 
-    else {
-      while(! triangleNumbers.isDefinedAt(n - 1)) {
-        triangle(n - 1)
-      }
-      triangleNumbers.insert(n, triangleNumbers(n - 1) + n)
-    }
-    
-    triangleNumbers(n)
-  }
+
 }
--- problem-012/src/test/scala/euler/AppSuite.scala
+++ problem-012/src/test/scala/euler/AppSuite.scala
@@ -4,34 +4,18 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 import org.junit.Assert._
+import euler.TriangleNumber
 
 
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends FunSuite {
 
   test("The 1st triangle number is 1") {
-    assertEquals(1, App.triangle(1))
+    val triangle = new TriangleNumber()
+    assertEquals(1, triangle(1))
   }
 
 
-  test("The 2nd triangle number is 3") {
-    assertEquals(3, App.triangle(2))
-  }  
-
-
-  test("The 3rd triangle number is 6") {
-    assertEquals(6, App.triangle(3))
-  }
-
-
-  test("The 4th triangle number is 10") {
-    assertEquals(10, App.triangle(4))
-  }
-  
-  test("The 10th triangle number is 55") {
-    assertEquals(55, App.triangle(10))
-    assertEquals(45, App.triangle(9))   // this should be pre-cached because of our call to triangle(10)
-  }
 }
 
 
