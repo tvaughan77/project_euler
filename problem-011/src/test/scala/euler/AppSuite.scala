ead32298d32b934aa592f66186c32d9a97882e62
tom.vaughan <tom_vaughan@hotmail.com>
2012-06-14 14:28:19 -0400

    Moving the extraction of diagonal data out of App.scala and into GridOperations.  Tests moving too

--- problem-011/src/test/scala/euler/AppSuite.scala
+++ problem-011/src/test/scala/euler/AppSuite.scala
@@ -21,54 +21,7 @@ class AppSuite extends FunSuite {
     assertEquals(0, App.computeGreatestProduct(Array(0, 0, 1, 0, 1, 0, 0), 2))
   }
   
-  test("The diagonal of my square test grid starting at (0,0) is (1, 5, 9)") {
-    val diagonal = App.returnDiagonal(testSquareArray, 0, 0)
-    assertEquals("The diagonal.size should be 3", 3, diagonal.size)
-    assertEquals("The diagonal(0) should be 1", 1, diagonal(0))
-    assertEquals("The diagonal(1) should be 5", 5, diagonal(1))
-    assertEquals("The diagonal(2) should be 9", 9, diagonal(2))
-  }
-  
-  test("The diagonal of my square test grid starting at (0,1) is (2, 6)") {
-    val diagonal = App.returnDiagonal(testSquareArray, 0, 1)
-    assertEquals(2, diagonal.size)
-    assertEquals(2, diagonal(0))
-    assertEquals(6, diagonal(1))
-  }
 
-  test("The diagonal of my square test grid starting at (1,0) is (4, 8)") {
-    val diagonal = App.returnDiagonal(testSquareArray, 1, 0)
-    assertEquals(2, diagonal.size)
-    assertEquals(4, diagonal(0))
-    assertEquals(8, diagonal(1))
-  }
-  
-  test("Starting with a too-high row results in errors") {
-    intercept[IllegalArgumentException] {
-      App.returnDiagonal(testSquareArray, 9, 0)
-    }
-  }
-  
-  
-  test("Starting with a too-high column results in errors") {
-    intercept[IllegalArgumentException] {
-      App.returnDiagonal(testSquareArray, 0, 9)
-    }
-  }
-  
-  test("The diagonal of my rectangular test grid starting at (0, 0) is (10, 14)") {
-    val diagonal = App.returnDiagonal(testRectangularArray, 0, 0)
-    assertEquals(2, diagonal.size)
-    assertEquals(10, diagonal(0))
-    assertEquals(14, diagonal(1))
-  }
-  
-  test("The diagonal of my rectangular test grid starting at (1, 0) is (13)") {
-    val diagonal = App.returnDiagonal(testRectangularArray, 1, 0)
-    assertEquals(1, diagonal.size)
-    assertEquals(13, diagonal(0))
-  }
-  
   test("The greatest product of diagonals on my square test grid is 1*5*9 = 45") {
     assertEquals(45, App.computeGreatestDiagProduct(testSquareArray, 3))
   }
