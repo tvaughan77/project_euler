4735cb7825e8ab1baf2b2a69a35c4c632db6b27d
tom.vaughan <tom_vaughan@hotmail.com>
2012-06-18 11:25:24 -0400

    This solution uses a HashMap as a cache, but that ends up blowing up my memory.  It also, for some reason, appears to blow up the stack, and I am really not sure why that is right now.

--- problem-014/src/main/scala/euler/App.scala
+++ problem-014/src/main/scala/euler/App.scala
@@ -1,10 +1,66 @@
 package euler.problem014
 
+import euler.LogHelper
+import scala.collection.mutable.HashMap
+
 /**
+ * <p>The following iterative sequence is defined for the set of positive integers:<br/>
+ * 
+ * n --> n/2 (n is even)    <br/>
+ * n --> 3n + 1 (n is odd)  <br/>
+ * </p>
+ * 
+ * <p>Using the rule above and starting with 13, we generate the following sequence:<br/>
+ * 13  40  20  10  5  16  8  4  2  1   <br/>
+ * <br/>
+ * It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. 
+ * Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.</p>
+ * 
+ * <p>Which starting number, under one million, produces the longest chain?</p>
+ * <p>NOTE: Once the chain starts the terms are allowed to go above one million.</p>
  */
-object App {
+object App extends LogHelper {
+  
+  val limit = 1000000
+  val cache = new HashMap[Int, List[Int]]
 
   def main(args: Array[String]) {
+    
+    var longestChain = 0
+    
+    for(i <- 1 to limit) {
+      try {
+        val size = chain(i).size
+        if(size > longestChain) {
+          info("New longest chain (%d) discovered by running number %d", size, i)
+          longestChain = size
+        }
+      } catch {
+        case ex: StackOverflowError => error("StackOverflowError caused by getting chain for " + i)
+      }
+    }
+  }
+  
+  def chain(x: Int): List[Int] = {
+    require(x > 0)
+    
+    def computeChain(x: Int): List[Int] = {
+      if(cache.isDefinedAt(x)) {
+        debug("computeChain hit cache for number %d", x)
+        cache(x)
+      }
+      else if(x == 1) 
+        List(1)
+      else if(x % 2 == 0) 
+        x :: computeChain(x / 2)
+      else 
+        x :: computeChain(3 * x + 1)
 
+    }
+    
+    debug("Computing chain for %d", x)    
+    if(!cache.isDefinedAt(x)) 
+      cache(x) = computeChain(x)
+    cache(x)
   }
 }
--- problem-014/src/test/scala/euler/AppSuite.scala
+++ problem-014/src/test/scala/euler/AppSuite.scala
@@ -9,8 +9,18 @@ import org.junit.Assert._
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends FunSuite {
 
-  test("The sum of 1+1=2") {
-    assertEquals(2, 1+1)
+  test("The chain starting at 13 is (13  40  20  10  5  16  8  4  2  1)") {
+    assertEquals(List(13, 40, 20, 10, 5, 16, 8, 4, 2, 1), App.chain(13))
+  }
+  
+  test("Cache should be hit") {
+    assertEquals(List(8, 4, 2, 1), App.chain(8))
+    assertEquals(List(16, 8, 4, 2, 1), App.chain(16))   // I should see this hit the cache
+  }
+  
+  test("Why does this hit a stack overflow error") {
+    val chain = App.chain(60415)
+    println("The chain for 60415 is " + chain)
   }
 }
 
