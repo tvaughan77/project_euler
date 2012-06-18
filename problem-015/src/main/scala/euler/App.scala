8196b70c95565fb48692370979d32e082573c38c
tom.vaughan <tom_vaughan@hotmail.com>
2012-06-18 15:23:47 -0400

    Setting up skeleton problem 15 project

---
+++ problem-015/src/main/scala/euler/App.scala
@@ -0,0 +1,13 @@
+package euler.problem015
+
+/**
+ * <p>Starting in the top left corner of a 22 grid, there are 6 routes (without backtracking)
+ * to the bottom right corner.</p>
+ * <p>How many routes are there through a 2020 grid?</p>
+ */
+object App {
+
+  def main(args: Array[String]) {
+
+  }
+}
