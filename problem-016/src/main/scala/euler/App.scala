a19f3fafc4638bedb878fa8585aeef7e070e8aed
tom.vaughan <tom_vaughan@hotmail.com>
2012-06-21 11:46:35 -0400

    Adding a solution for 2^1000 problem, leveraging the Numbers.carryTens solution I had written for problem013

--- problem-016/src/main/scala/euler/App.scala
+++ problem-016/src/main/scala/euler/App.scala
@@ -1,12 +1,42 @@
 package euler.problem016
 
 import euler.LogHelper
+import euler.Numbers
 
 /**
+ * <p>2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.<br/>
+ * What is the sum of the digits of the number 2^1000?</p>
+ * 
+ * Answer is = 1366
  */
 object App extends LogHelper {
 
   def main(args: Array[String]) {
-
+    info("The sum of the digits in the number 2^1000 is %d", pow(2, 1000).sum)
+  }
+  
+  /**
+   * @param base the number to raise to an {@code exponent}
+   * @param exponent the number to raise the {@code base} by
+   * @return the {@code base^exponent} value, represented as a List, where the head of the list is the
+   * most significant digit of the answer and the tail of the List is the one's place
+   */
+  def pow(base: Int, exponent: Int): List[Int] = {
+    var number = List(1)
+    for(i <- 1 to exponent) {
+      number = multiply(number, base)
+      debug("%d^%d is = %s", base, i, number)
+    }
+    number
+  }
+  
+  /**
+   * @param num a number, represented by a List where the head of the list is the most significant figure of
+   * the number and the tail is the 1's place of the number.  Assumes base10
+   * @param by the number to multiply {@code num} by
+   * @return a new list that contains the result of multiplying {@code num} by {@code by}
+   */
+  def multiply(value: List[Int], by: Int): List[Int] = {
+    Numbers.carryTens(value map (_ * by))
   }
 }
--- problem-016/src/test/scala/euler/AppSuite.scala
+++ problem-016/src/test/scala/euler/AppSuite.scala
@@ -9,8 +9,24 @@ import org.junit.Assert._
 @RunWith(classOf[JUnitRunner])
 class AppSuite extends FunSuite {
 
-  test("The sum of 1+1=2") {
-    assertEquals(2, 1+1)
+  test("The multiple of List(1, 2, 3) by 2 is List(2, 4, 6)") {
+    assertEquals(List(2, 4, 6), App.multiply(List(1, 2, 3), 2))
+  }
+  
+  test("The multiple of List(2, 5, 6) by 2 is List(5, 1, 2)") {
+    assertEquals(List(5, 1, 2), App.multiply(List(2, 5, 6), 2))
+  }
+  
+  test("The multiple of List(5, 1, 2) by 2 is List(1, 0, 2, 4)") {
+    assertEquals(List(1, 0, 2, 4), App.multiply(List(5, 1, 2), 2))
+  }
+  
+  test("Raising 2^5 is List(3, 2)") {
+    assertEquals(List(3, 2), App.pow(2, 5))
+  }
+  
+  test("Raising 3^10 is List(5, 9, 0, 4, 9)") {
+    assertEquals(List(5, 9, 0, 4, 9), App.pow(3, 10))
   }
 }
 
